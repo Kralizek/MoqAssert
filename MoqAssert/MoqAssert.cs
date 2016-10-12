1434b0ad302c4007119d5e08ef69292d1910e245
Renato Golia <renato.golia@studentum.se>
2016-10-12 14:00:43 +0200

    Changed namespace to Moq

--- MoqAssert/MoqAssert.cs
+++ MoqAssert/MoqAssert.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Moq;
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
 
-namespace MoqAssert
+// ReSharper disable CheckNamespace
+
+namespace Moq
 {
     public static class MoqAssert
     {
